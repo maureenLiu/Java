6a67546020cdd766e2ac40b2dcdd7b0badd4b1cd
Administrator <Administrator@A5V0RWU0CEG6LQV>
2020-02-24 16:13:36 +0800

    添加键盘监听事件

--- tank/src/com/maureen/tank/TankFrame.java
+++ tank/src/com/maureen/tank/TankFrame.java
@@ -2,6 +2,8 @@ package com.maureen.tank;
 
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -14,13 +16,14 @@ public class TankFrame extends Frame {
 		setTitle("Tank war");
 		setVisible(true);
 		
+		this.addKeyListener(new MyKeyListener());
+		
 		addWindowListener(new WindowAdapter() {
 
 			@Override
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 			}
-			
 		});
 	}
 
@@ -31,4 +34,17 @@ public class TankFrame extends Frame {
 		y += 10;
 	}
 	
+	class MyKeyListener extends KeyAdapter {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			System.out.println("keyPressed");
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			System.out.println("keyReleased");
+		}
+		
+	}
 }
