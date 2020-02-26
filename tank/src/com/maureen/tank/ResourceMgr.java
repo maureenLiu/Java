0c1d3fdc45c6fae5598aed261927ff7f08876bf6
maureen <1437867946@qq.com>
2020-02-26 20:10:29 +0800

    我方(好)坦克和敌方(坏)坦克的图片区分

--- tank/src/com/maureen/tank/ResourceMgr.java
+++ tank/src/com/maureen/tank/ResourceMgr.java
@@ -6,17 +6,23 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class ResourceMgr {
-	public static BufferedImage tankL, tankU, tankR,tankD;
+	public static BufferedImage goodTankL, goodTankU, goodTankR, goodTankD;
+	public static BufferedImage badTankL, badTankU, badTankR, badTankD;
 	public static BufferedImage bulletL, bulletU, bulletR,bulletD;
 	//爆炸过程有16张图
 	public static BufferedImage[] explodes = new BufferedImage[16]; 
 	
 	static { //当ResourceMgr.class文件被load到内存的时候，静态语句块自动执行，BufferedImage就被初始化了
 		try { //图片全部被load到了内存中
-			tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/BadTank1.png"));
-			tankL = ImageUtil.rotateImage(tankU,-90);
-			tankR = ImageUtil.rotateImage(tankU,90);
-			tankD = ImageUtil.rotateImage(tankU,180);
+			goodTankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/GoodTank1.png"));
+			goodTankL = ImageUtil.rotateImage(goodTankU,-90);
+			goodTankR = ImageUtil.rotateImage(goodTankU,90);
+			goodTankD = ImageUtil.rotateImage(goodTankU,180);
+			
+			badTankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/BadTank1.png"));
+			badTankL = ImageUtil.rotateImage(badTankU,-90);
+			badTankR = ImageUtil.rotateImage(badTankU,90);
+			badTankD = ImageUtil.rotateImage(badTankU,180);
 		
 			bulletU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletU.png"));
 			bulletL = ImageUtil.rotateImage(bulletU,-90);
--- tank/src/com/maureen/tank/Tank.java
+++ tank/src/com/maureen/tank/Tank.java
@@ -6,9 +6,8 @@ import java.util.Random;
 
 public class Tank {
 	private static final int SPEED = 3;
-	public static int WIDTH = ResourceMgr.tankD.getWidth();
-
-	public static int HEIGHT = ResourceMgr.tankD.getHeight();
+	public static int WIDTH = ResourceMgr.goodTankD.getWidth();
+	public static int HEIGHT = ResourceMgr.goodTankD.getHeight();
 
 	private Random random = new Random();
 	private int x, y;
@@ -101,16 +100,16 @@ public class Tank {
 
 		switch (dir) {
 		case LEFT:
-			g.drawImage(ResourceMgr.tankL, x, y, null);
+			g.drawImage(this.group == Group.GOOD? ResourceMgr.goodTankL:ResourceMgr.badTankL, x, y, null);
 			break;
 		case UP:
-			g.drawImage(ResourceMgr.tankU, x, y, null);
+			g.drawImage(this.group == Group.GOOD? ResourceMgr.goodTankU:ResourceMgr.badTankU, x, y, null);
 			break;
 		case RIGHT:
-			g.drawImage(ResourceMgr.tankR, x, y, null);
+			g.drawImage(this.group == Group.GOOD? ResourceMgr.goodTankR:ResourceMgr.badTankR, x, y, null);
 			break;
 		case DOWN:
-			g.drawImage(ResourceMgr.tankD, x, y, null);
+			g.drawImage(this.group == Group.GOOD? ResourceMgr.goodTankD:ResourceMgr.badTankD, x, y, null);
 			break;
 		}
 
