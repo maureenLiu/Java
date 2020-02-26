c39a6c105fadc1fd33efdebb31f648a31b86152b
maureen <1437867946@qq.com>
2020-02-26 15:14:57 +0800

    子弹换成图片

--- tank/src/com/maureen/tank/ResourceMgr.java
+++ tank/src/com/maureen/tank/ResourceMgr.java
@@ -7,6 +7,7 @@ import javax.imageio.ImageIO;
 
 public class ResourceMgr {
 	public static BufferedImage tankL, tankU, tankR,tankD;
+	public static BufferedImage bulletL, bulletU, bulletR,bulletD;
 	
 	static { //当ResourceMgr.class文件被load到内存的时候，静态语句块自动执行，BufferedImage就被初始化了
 		try { //四张图片全部被load到了内存中
@@ -14,6 +15,11 @@ public class ResourceMgr {
 			tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
 			tankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
 			tankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
+			
+			bulletL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletL.gif"));
+			bulletU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletU.gif"));
+			bulletR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletR.gif"));
+			bulletD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
