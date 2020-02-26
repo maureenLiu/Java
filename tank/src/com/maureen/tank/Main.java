3031541035898e45d49c72be5053f3c9d5502cb9
maureen <1437867946@qq.com>
2020-02-26 15:39:19 +0800

    new出5个敌人坦克

--- tank/src/com/maureen/tank/Main.java
+++ tank/src/com/maureen/tank/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) throws InterruptedException {
 		TankFrame tf = new TankFrame();
 		
+		//初始化地方坦克
+		for(int i = 0; i < 5; i++) {
+			tf.enemies.add(new Tank(50 + i*80, 200, Dir.DOWN, tf));
+		}
 		while(true) {
 			Thread.sleep(50);
 			tf.repaint();
--- tank/src/com/maureen/tank/TankFrame.java
+++ tank/src/com/maureen/tank/TankFrame.java
@@ -14,11 +14,13 @@ import java.util.List;
 
 public class TankFrame extends Frame {
 	
-	Tank myTank = new Tank(200,200,Dir.DOWN, this);
+	Tank myTank = new Tank(200,400,Dir.DOWN, this);
 	List<Bullet> bullets = new ArrayList<>();
-	Bullet bullet = new Bullet(300,300,Dir.DOWN,this);
+	List<Tank> enemies = new ArrayList<>();
 	static final int GAME_WIDTH = 800;
 	static final int GAME_HEIGHT = 600;
+	
+	
 	
 	public TankFrame( ) {
 		setSize(GAME_WIDTH,GAME_HEIGHT);
@@ -60,9 +62,13 @@ public class TankFrame extends Frame {
 		g.setColor(c);
 		
 		myTank.paint(g);
-		for(int i = 0; i< bullets.size(); i++) {
+		for(int i = 0; i < bullets.size(); i++) {
 			bullets.get(i).paint(g);
 		} 
+		
+		for(int i = 0; i < enemies.size(); i++) {
+			enemies.get(i).paint(g);
+		}
 		
 //		for(Iterator<Bullet> it = bullets.iterator(); it.hasNext();) {
 //			Bullet b = it.next();
