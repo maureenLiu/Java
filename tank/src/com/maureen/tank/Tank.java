66f20dd7b1288ba926887c308b583a39f5069db3
maureen <1437867946@qq.com>
2020-02-26 19:52:22 +0800

    敌人坦克随机移动

--- tank/src/com/maureen/tank/Tank.java
+++ tank/src/com/maureen/tank/Tank.java
@@ -84,8 +84,14 @@ public class Tank {
 			break;
 		}
 
-		if (random.nextInt(10) > 8)
+		if (this.group == Group.BAD && random.nextInt(100) > 95)
 			this.fire();
+		if (this.group == Group.BAD && random.nextInt(100) > 95)
+			randomDir();
+	}
+
+	private void randomDir() {
+		this.dir = Dir.values()[random.nextInt(4)];
 
 	}
 
