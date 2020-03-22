4f8d72bb2cf94246fbcce932c10b65e7b43c8bd5
maureenLiu <1437867946@qq.com>
2020-03-22 16:59:15 +0800

    学习使用SimpleChannelInboundHandler

--- tank/src/com/maureen/tank/net/Client.java
+++ tank/src/com/maureen/tank/net/Client.java
@@ -69,14 +69,15 @@ class ClientChannelInitializer extends ChannelInitializer<SocketChannel> {
     }
 }
 
-class ClientChannelHandler extends ChannelInboundHandlerAdapter {
+class ClientChannelHandler extends SimpleChannelInboundHandler<TankJoinMsg> {
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         ctx.writeAndFlush(new TankJoinMsg(TankFrame.INSTANCE.getMainTank()));
     }
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println(msg);
-    }
+	@Override
+	protected void channelRead0(ChannelHandlerContext ctx, TankJoinMsg msg) throws Exception {
+		System.out.println(msg);
+		
+	}
 }
