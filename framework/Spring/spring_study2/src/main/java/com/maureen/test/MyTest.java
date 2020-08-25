a5c5a17a949c840bd5c5ea7893add19c71a79919
Maureen <1437867946@qq.com>
2020-08-25 14:28:17 +0800

    Spring对象的获取：1、通过bean的id标签 2、通过bean的类型（但是同类型的对象无法完成）

--- framework/Spring/spring_study2/src/main/java/com/maureen/test/MyTest.java
+++ framework/Spring/spring_study2/src/main/java/com/maureen/test/MyTest.java
@@ -7,7 +7,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyTest {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("ioc.xml");
+        /**
+         * Spring对象的获取及属性赋值方式
+         * 1、根据bean标签的id来获取对象
+         */
         Person person = context.getBean("person", Person.class);
         System.out.println(person);
+
+        /**
+         * 2、根据bean的类型来获取对象
+         * 注意：当通过类型进行获取的时候，如果存在两个相同类型对象，将无法完成获取工作
+         */
+        Person bean = context.getBean(Person.class);
+        System.out.println(bean);
     }
 }
